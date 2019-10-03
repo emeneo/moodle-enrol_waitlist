b92a6fffa046f3292ba8987f2e4b83c233306d9f
Flotter Totte <flottertotte@emeneo.com>
2019-10-03 08:39:21 +0800

    GDPR Compliance

--- version.php
+++ version.php
@@ -28,6 +28,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version  = 2018110701;   // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2011033005;   // Requires at least this Moodle version
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'Course Enrol Waitlist Plugin Version 3.5-f';
+$plugin->release = 'Course Enrol Waitlist Plugin Version 3.5-h';
 $plugin->component = 'enrol_waitlist'; // Full name of the plugin (used for diagnostics)
 $plugin->cron = 180;
