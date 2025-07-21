69431bb1604763ba2359a5314594de94e5704613
Flotter Totte <flottertotte@emeneo.com>
2025-07-21 12:36:59 +0200

    Update version.php

--- version.php
+++ version.php
@@ -28,6 +28,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version  = 2025072000;   // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2024083000;   // Requires at least this Moodle version
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'Course Enrol Waitlist Plugin Version 4.5-a';
+$plugin->release = 'Course Enrol Waitlist Plugin Version 4.5-c';
 $plugin->component = 'enrol_waitlist'; // Full name of the plugin (used for diagnostics)
 $plugin->cron = 180;
